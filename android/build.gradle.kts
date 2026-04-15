1ec9fe5c2590f54b5cc2622332c17e30eb89beeb
abohaedrmaher45-ux <abohaedrmaher45@gmail.com>
2026-04-15 18:10:31 +0300

    باب1ا

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,16 @@
+// android/build.gradle.kts
+
 buildscript {
-    ext.kotlin_version = '2.1.0'  // ✅ تم التحديث
+    val kotlin_version = "2.1.0"
+    
     repositories {
         google()
         mavenCentral()
     }
 
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.7.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:8.7.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
@@ -18,14 +21,18 @@ allprojects {
     }
 }
 
-rootProject.buildDir = '../build'
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
 subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
-}
-subprojects {
-    project.evaluationDependsOn(':app')
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
-tasks.register("clean", Delete) {
-    delete rootProject.buildDir
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,12 @@
+// android/settings.gradle.kts
+
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
-        val propertiesFile = file("local.properties")
-        if (propertiesFile.exists()) {
-            propertiesFile.inputStream().use { properties.load(it) }
-        }
-        val settingsSdkPath = properties.getProperty("flutter.sdk")
-        settingsSdkPath ?: System.getenv("FLUTTER_ROOT") ?: throw GradleException("Flutter SDK not found. Define flutter.sdk in local.properties or set FLUTTER_ROOT env variable.")
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -21,7 +21,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // ✅ تم التحديث من 2.0.21 إلى 2.1.0
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
